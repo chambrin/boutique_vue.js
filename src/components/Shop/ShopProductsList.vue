ea858247a543a1cb5b7807b04637552a96c90915
Chambrin <achambrin@outlook.fr>
2021-11-11 17:57:37 +0100

    version0.10

---
+++ src/components/Shop/ShopProductsList.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="d-flex felx-row flex-wrap">
+  <shop-products-item> </shop-products-item>
+  <shop-products-item> </shop-products-item>
+  <shop-products-item> </shop-products-item>
+  <shop-products-item> </shop-products-item>
+  <shop-products-item> </shop-products-item>
+  <shop-products-item> </shop-products-item>
+  <shop-products-item> </shop-products-item>
+  <shop-products-item> </shop-products-item>
+</div>
+</template>
+
+
+
+
+
+
+
+
+
+<script>
+import ShopProductsItem from "./ShopProductItem.vue";
+
+export default {
+  components: {
+    ShopProductsItem,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" >
+      <img src="../assets/logo.png" alt="logo de du site" widht="30" height="30" >
+      Logo
+    </a>
+    <button class="navbar-toggler">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="navbar-item">
+            <a href="" class="nav-link">Boutique</a>        
+          </li>
+          <li class="navbar-item">
+            <a href="" class="nav-link">Admin</a>        
+          </li>
+        </ul>
+      </div>
+  </nav>
+</template>
+
+
+
+
+
+
+<script>
+
+
+export default {
+
+
+  }
+
+
+
+
+
+
+</script>
+
+
+
+
+
+
+<style>
+
+</style>
